d8c472a29691a7ea64d0646b4bd93afa2cccf654
Mads Hougesen <madshougesen@gmail.com>
2021-10-05 22:11:23 +0200

    feat: created model for Github user

---
+++ Portfolio/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Portfolio.Models
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProfileImage { get; set; }
+
+        public string Company { get; set; }
+
+        public string Location { get; set; }
+
+        public string GithubUserName { get; set; }
+
+        public string TwitterUserName { get; set; }
+
+    }
+}
